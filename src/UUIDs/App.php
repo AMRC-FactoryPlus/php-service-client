33c1ad4f1182fc1ad7864fe95d8ab64dafaefd0b
Alex Godbehere <alex.godbehere@gmail.com>
2023-11-28 15:47:19 +0000

    Add EdgeClusterConfiguration to UUID/App

--- src/UUIDs/App.php
+++ src/UUIDs/App.php
@@ -14,4 +14,6 @@ class App
     const EdgeAgentConfig = "aac6f843-cfee-4683-b121-6943bfdf9173";
     const EdgeAgentDeployment = "f2b9417a-ef7f-421f-b387-bb8183a48cdb";
     const EdgeClusterStatus = "747a62c9-1b66-4a2e-8dd9-0b70a91b6b75";
+    const EdgeClusterConfiguration = "bdb13634-0b3d-4e38-a065-9d88c12ee78d";
+
 }
